542889dc8e32b97dfae6e9878c8d7dfc7a179914
Wojciech <heiw17@wp.pl>
2023-09-22 23:06:35 +0200

    express-async-handler

--- backend/controller/controller.js
+++ backend/controller/controller.js
@@ -1,4 +1,7 @@
-const auth = (req, res) => {
+// library "express-async-handler let user to not use try/catch"
+const asyncHandler = require("express-async-handler");
+
+const auth = asyncHandler(async (req, res) => {
   res.status(200).json({ m: "hopefully it works" });
-};
+});
 module.exports = { auth };
